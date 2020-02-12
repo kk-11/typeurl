19eb0e1b9e99289994cd6ceccf035e83e842fa52
killian <killian.debuitleir@gmail.com>
2020-02-12 21:18:21 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.editURL = this.editURL.bind(this);
+		this.loop = this.loop.bind(this);
+		this.state = {
+			counter: 0
+		}
+		this.loopWord = `He's......behind......you`.split('');
+	}
+	componentDidMount() {
+		window.location.hash = '';
+		this.loop();
+	}
+
+	loop(minT, maxT) {
+		var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+		setTimeout(() => {
+			this.editURL();
+			this.loop();
+		}, rand);
+	}
+
+	editURL() {
+		if (this.state.counter >= this.loopWord.length) {
+			window.location.hash = '';
+			return;
+		}
+		window.location.hash = `${window.location.hash}${this.loopWord[this.state.counter]}`;
+		this.setState({
+			counter: this.state.counter + 1
+		})
+	}
+	componentWillUnmount() {
+		clearTimeout();
+	}
+
+	render() {
+		return (
+			<div>
+				He's......behind......you
+			</div>
+		);
+	}
 }
-
-export default App;
